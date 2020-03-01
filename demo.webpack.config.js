9f3738efa9208297743871bcc48f5d2a2bdebe33
atokulus <IgitIgit4ever>
2020-03-01 13:38:31 +0100

    WIP demonstration webpage

--- demo.webpack.config.js
+++ demo.webpack.config.js
@@ -54,10 +54,13 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
             loader: 'file-loader',
+            options: {
+              esModule: false,
+            },
           },
         ],
       }
--- demo/components/App.vue
+++ demo/components/App.vue
@@ -1,21 +1,49 @@
 <template>
-  <div class="app-body">
-    <ar-view v-if="this.show">
-      <ar-element :id="23">
-        <div class="hello hello1">Hello World!</div>
-        <div class="hello hello2">Hallo Welt!</div>
-      </ar-element>
-      <ar-element :id="50" v-slot:="{tracked}">
-        <div class="outer" :class="{tracked: tracked}">
-          <div class="inner">
-            Other Tag!
-          </div>
+  <main class="app-body">
+    <header>
+      <a href="https://www.npmjs.com/package/vuexr" target="_blank">npm</a>
+      <a href="https://github.com/technokrat-llc/vuexr" target="_blank">GitHub</a>
+      <a href="https://technokrat.ch" target="_blank">Technokrat</a>
+    </header>
+    <div class="lead">
+      <img src="../../doc/vuexr_illustration.svg" style="width: 500px; max-width: 100%;">
+      <h1>VueXR</h1>
+      <p>Vue-powered Augmented Reality DOM rendering</p>
+      <a href="https://github.com/technokrat-llc/vuexr#getting-started" target="_blank">Getting Started</a>
+    </div>
+    <div class="demo">
+        <div v-if="this.step === 'selection'">
+          <h2>Demo</h2>
+          <p>Open a AR view on one device with a camera, and display the guide on another one (preferably a notebook or desktop computer).</p>
+          <a href="" @click.prevent="step = 'view'">Open AR view</a> or <a href="" @click.prevent="step = 'calibration'">Display Guide</a>
         </div>
-      </ar-element>
-    </ar-view>
-    <ar-chessboard></ar-chessboard>
-<!--    <button @click="this.toggle">Toggle</button>-->
-  </div>
+        <div v-else-if="this.step === 'view'">
+          <ar-view v-if="supported">
+            <ar-element :id="42" v-slot:="{tracked}">
+              <div class="hello">
+                <h1>VueXR says hello!</h1>
+                <p>Embed any reactive Vue component in this floating element.</p>
+              </div>
+            </ar-element>
+          </ar-view>
+          <h3 v-else>Augmented Reality is not supported on this device. Do you have a camera?</h3>
+        </div>
+        <div v-else-if="this.step === 'calibration'">
+          <h2>Calibration</h2>
+          <p>Follow the calibration procedure on your AR device. Use the chessboard pattern for calibration below.</p>
+          <ar-chessboard />
+          <a href="" @click.prevent="step = 'selection'">Back</a> <a href="" @click.prevent="step = 'detection'">Ready</a>
+        </div>
+        <div v-else-if="this.step === 'detection'">
+          <h2>Detection</h2>
+          <img src="../assets/6x6_1000-42.svg" style="max-width: 100%;">
+          <a href="" @click.prevent="step = 'calibration'">Back</a>
+        </div>
+    </div>
+    <footer>
+      MIT Licensed | Copyright © 2020-present Markus Wegmann, <a href="https://technokrat.ch" target="_blank">Technokrat LLC</a>
+    </footer>
+  </main>
 </template>
 
 <script>
@@ -24,7 +52,8 @@
   const App = Vue.extend({
     data () {
       return {
-        show: true
+        supported: false,
+        step: 'selection'
       }
     },
     methods: {
@@ -32,12 +61,21 @@
         this.show = !this.show;
       }
     },
+    mounted () {
+      this.$vuexr.supported().then((supported) => {
+        this.supported = supported;
+      }).catch(() => {
+        this.supported = false;
+      })
+    }
   });
 
   export default App;
 </script>
 
 <style>
+  @import "~normalize.css/normalize.css";
+
   html, body {
     padding: 0;
     height: 100%;
@@ -48,7 +86,7 @@
   }
 
   .app-body {
-    height: 100%;
+    min-height: 100%;
   }
 
   .hello {
