5c8354f8a782c925e9d9bf25052c8d512ac05158
Markus Wegmann <mw@siglis.com>
2022-07-19 21:15:44 +0200

    Fix build

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,14 +1,8 @@
 import {VueXRManager} from "./VueXRManager"
 
-declare module "*.svg" {
-  export default content;
-}
-
 declare module "@vue/runtime-core" {
   //Bind to `this` keyword
   interface ComponentCustomProperties {
     $vuexr: VueXRManager;
   }
 }
-
-
