531731a182bf4b1ba020aec48b35719a78814c46
Markus Wegmann <mw@technokrat.ch>
2023-01-30 14:51:46 +0100

    Try to fix pattern import

--- src/components/ARChessboard.vue
+++ src/components/ARChessboard.vue
@@ -1,16 +1,20 @@
 <template>
-  <img alt="Chessboard pattern for camera calibration" :src="pattern" style="max-width: 100%; max-height: 100%">
+  <img
+    alt="Chessboard pattern for camera calibration"
+    :src="pattern"
+    style="max-width: 100%; max-height: 100%"
+  />
 </template>
 
 <script>
-  import { defineComponent } from "vue";
-  import pattern from '../vision/Chessboard'
+import { defineComponent } from "vue";
+import pattern from "../vision/Chessboard";
 
-  export default defineComponent({
-    data: function () {
-      return {
-        pattern: pattern
-      }
-    }
-  });
+export default defineComponent({
+  data: function () {
+    return {
+      pattern: pattern,
+    };
+  },
+});
 </script>
